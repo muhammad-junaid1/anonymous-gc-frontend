2f4f0c919f037cc70e9b4fbbf7bd399ce19f7dcf
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-12-16 20:32:42 +0500

    online users modal

--- src/components/chat/ChatHeader.jsx
+++ src/components/chat/ChatHeader.jsx
@@ -1,38 +1,20 @@
-import { IconButton } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 import { useEffect, useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
-import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 import axios from "../../axiosConfig";
 import { useStateContext } from "../../ContextProvider";
 import { socket } from "../../App";
+import { IoMdEye } from "react-icons/io";
+import OnlineUsersModal from "./OnlineUsersModal";
+
+const totalUsersToView = 2;
 
 const ChatHeader = () => {
   const { User } = useStateContext();
-  const [users, setUsers] = useState([]);
-  const [onlineUsers, setOnlineUsers] = useState(0);
-  const navigate = useNavigate();
+  const [onlineUsers, setOnlineUsers] = useState([]);
   const [isTyping, setIsTyping] = useState("");
-
-  const fetchUsers = async () => {
-    try {
-      const response = await axios.get("/users");
-      const users = response?.data?.data;
-      setUsers(users);
-    } catch (error) {
-      console.log(error);
-      toast.error("Something went wrong", {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-  };
+  const [onlineUsersModal, setOnlineUsersModal] = useState(false);
 
   const fetchOnlineUsers = async () => {
     try {
@@ -42,7 +24,7 @@ const ChatHeader = () => {
 
       if (socket) {
         socket.on("chat_getOnlineUsers", (users) => {
-          setOnlineUsers(users?.length);
+          setOnlineUsers(users);
         });
       }
     } catch (error) {
@@ -60,9 +42,12 @@ const ChatHeader = () => {
     }
   };
 
+  const handleShowOnlineUsers = () => {
+    setOnlineUsersModal(true);
+  };
+
   useEffect(() => {
     if (User?.role === 1) {
-      fetchUsers();
       fetchOnlineUsers();
 
       if (socket) {
@@ -82,63 +67,78 @@ const ChatHeader = () => {
     }
   }, []);
   return (
-    <div className="chat-header shadow border-b border-gray-300 px-3 py-2 flex items-center justify-between">
-      {User?.role === 1 && (
-        <div className="relative mt-1">
-          {users?.slice(0, 6)?.map((user, index) => (
-            <div
-              style={{
-                transform: isTyping ? "translateY(-70%)" : "translateY(-50%)",
-                left: index * 15,
-              }}
-              key={user?._id}
-              className={`image-container absolute ${
-                isTyping ? "top-[30%]" : "top-[50%]"
-              } w-[28px] h-[28px]`}
-            >
-              <img
-                src={user?.profile_picture}
-                className="round-image border border-black"
-                alt=""
-              />
-            </div>
-          ))}
-          {users?.length > 6 && (
-            <p
-              onClick={() => navigate("/employees")}
-              className={`absolute cursor-pointer w-max text-sm ${
-                isTyping ? "top-[30%]" : "top-[50%]"
-              }`}
-              style={{
-                left: 7 * 16,
-                transform: isTyping ? "translateY(-70%)" : "translateY(-50%)",
-              }}
-            >
-              {users?.length - 6} more
-            </p>
-          )}
-          {!!isTyping && (
-            <p className={`absolute fadeIn text-xs text-primary left-0 w-max -bottom-[28px]`}>
-              {isTyping} is typing...
+    <>
+      <div className="chat-header shadow border-b border-gray-300 px-3 py-2 flex items-center justify-between">
+        {User?.role === 1 && (
+          <div className="relative mt-1">
+            {onlineUsers?.slice(0, totalUsersToView)?.map((user, index) => (
+              <div
+                style={{
+                  transform: isTyping ? "translateY(-70%)" : "translateY(-50%)",
+                  left: index * 15,
+                }}
+                key={user?._id}
+                className={`image-container absolute ${
+                  isTyping ? "top-[30%]" : "top-[50%]"
+                } w-[28px] h-[28px]`}
+              >
+                <img
+                  src={user?.profile_picture}
+                  className="round-image border border-black"
+                  alt=""
+                />
+              </div>
+            ))}
+            {
+              <Tooltip
+                className={`cursor-pointer w-max text-sm ${
+                  isTyping ? "top-[30%]" : "top-[50%]"
+                }`}
+                style={{
+                  position: "absolute",
+                  left: (totalUsersToView + 1) * 16,
+                  transform: isTyping ? "translateY(-70%)" : "translateY(-50%)",
+                }}
+                title="See Online users"
+              >
+                <IconButton onClick={handleShowOnlineUsers}>
+                  <IoMdEye size={20} />
+                </IconButton>
+              </Tooltip>
+            }
+            {!!isTyping && (
+              <p
+                className={`absolute fadeIn text-xs text-primary left-0 w-max -bottom-[28px]`}
+              >
+                {isTyping} is typing...
+              </p>
+            )}
+          </div>
+        )}
+        <div className="flex flex-1 text-center flex-col items-center">
+          <p className="font-extralight text-lg mt-1">Chat Group</p>
+          {User?.role === 1 && (
+            <p className="text-sm font-bold mb-0">
+              {onlineUsers?.length}{" "}
+              <span className="text-green-500">Online</span>
             </p>
           )}
         </div>
+        <div>
+          <IconButton>
+            <BsThreeDots size={22} style={{ color: "black" }} />
+          </IconButton>
+        </div>
+      </div>
+
+      {!!onlineUsersModal && (
+        <OnlineUsersModal
+          onlineUsers={onlineUsers}
+          onlineUsersModal={onlineUsersModal}
+          handleClose={() => setOnlineUsersModal(false)}
+        />
       )}
-      <div className="flex flex-1 text-center flex-col items-center">
-        <p className="font-extralight text-lg mt-1">Chat Group</p>
-        {User?.role === 1 && (
-          <p className="text-sm font-bold mb-0">
-            {onlineUsers ? onlineUsers - 1 : onlineUsers}{" "}
-            <span className="text-green-500">Online</span>
-          </p>
-        )}
-      </div>
-      <div>
-        <IconButton>
-          <BsThreeDots size={22} style={{ color: "black" }} />
-        </IconButton>
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/chat/OnlineUsersModal.jsx
@@ -0,0 +1,53 @@
+import { Modal, Backdrop, IconButton } from "@mui/material";
+import { IoMdClose } from "react-icons/io";
+import UserCard from "../UserCard";
+
+const style = {
+  transform: "translate(-50%, -50%)",
+  boxShadow: 24,
+};
+
+const OnlineUsersModal = ({ onlineUsersModal, onlineUsers, handleClose }) => {
+  return (
+    <Modal
+      keepMounted
+      open={onlineUsersModal}
+      onClose={handleClose}
+      aria-labelledby="keep-mounted-modal-title"
+      aria-describedby="keep-mounted-modal-description"
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <div
+        style={style}
+        className={`w-[calc(100%-20px)] md:w-[70%] overflow-y-scroll h-[70%] bg-white absolute top-1/2 left-1/2 px-5 pt-8 pb-6 rounded-md`}
+      >
+        <IconButton
+          sx={{
+            position: "absolute",
+            right: 7,
+            top: 10,
+            color: "black",
+          }}
+          onClick={handleClose}
+        >
+          <IoMdClose size={18} />
+        </IconButton>
+
+        <h1 className="text-2xl font-bold text-center mt-4">Online</h1>
+
+          {!!(onlineUsers?.length === 0) && <div className="flex items-center h-[70%] justify-center text-gray-500 font-bold">No one is Online at the moment.</div>}
+        <div className="flex mt-5 flex-wrap gap-2">
+          {onlineUsers?.map((user) => (
+            <UserCard key={user?._id} data={user} width={48} />
+          ))}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default OnlineUsersModal;
