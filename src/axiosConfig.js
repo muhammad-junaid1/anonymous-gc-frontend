afc5f02b9b9d568351e1bb20598a2d85b7ef3be6
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-12-17 16:01:28 +0500

    latest

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -5,6 +5,7 @@ axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 const instance = axios.create({
     headers: {
         "Content-Type": "application/json", 
+        "ngrok-skip-browser-warning": "true",
         "Authorization": `Bearer ${localStorage.getItem("auth-token")}`
     }
 })
